e367723477676dc0b41dd55ce4fe604bec8e3ab8
LailyNurH <lailyhamidah3005@gmail.com>
2023-12-22 15:27:58 +0700

    add detail payment activity

--- app/src/main/java/com/programmer/finalproject/ui/bottomsheet/PremiumBottomSheet.kt
+++ app/src/main/java/com/programmer/finalproject/ui/bottomsheet/PremiumBottomSheet.kt
@@ -1,5 +1,6 @@
 package com.programmer.finalproject.ui.bottomsheet
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -9,19 +10,25 @@ import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import coil.load
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.chip.Chip
 import com.google.android.material.tabs.TabLayoutMediator
+import com.programmer.finalproject.R
 import com.programmer.finalproject.adapter.PagerAdapter
 import com.programmer.finalproject.databinding.FilterBottomSheetBinding
 import com.programmer.finalproject.databinding.PremiumBottomSheetBinding
 import com.programmer.finalproject.model.detailcourse.DetailCourseResponse3
+import com.programmer.finalproject.model.payment.OrderRequest
 import com.programmer.finalproject.ui.DetailKelasActivity.Companion.COURSE_ID
+import com.programmer.finalproject.ui.fragment.DetailPaymentActivity
+import com.programmer.finalproject.ui.fragment.auth.AuthViewModel
 import com.programmer.finalproject.ui.fragment.detailkelas.DetailKelasViewModel
 import com.programmer.finalproject.ui.fragment.detailkelas.MateriKelasFragment
 import com.programmer.finalproject.ui.fragment.detailkelas.TentangKelasFragment
 import com.programmer.finalproject.ui.fragment.kursus.FilterViewModel
+import com.programmer.finalproject.ui.orders.OrdersViewModel
 import com.programmer.finalproject.utils.NetworkResult
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -31,6 +38,8 @@ class PremiumBottomSheet : BottomSheetDialogFragment() {
     private lateinit var binding : PremiumBottomSheetBinding
     private val detailKelasViewModel: DetailKelasViewModel by viewModels()
 
+    private val authViewModel: AuthViewModel by viewModels()
+    private val orderViewModel: OrdersViewModel by viewModels()
 
 
     override fun onCreateView(
@@ -47,7 +56,32 @@ class PremiumBottomSheet : BottomSheetDialogFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         requestDetailClassFromApi()
+
+        binding.btnOrderCourse.setOnClickListener {
+            orderCourse()
+        }
     }
+
+    private fun orderCourse() {
+            authViewModel.token.observe(viewLifecycleOwner){ it ->
+                if (it != null) {
+                    val orderRequest = OrderRequest(COURSE_ID)
+                    orderViewModel.orderCourses("Bearer $it",orderRequest)
+                    orderViewModel.isError.observe(viewLifecycleOwner){isError->
+                        if(isError){
+                            Toast.makeText(requireContext(), "gagal melakukan order kursus", Toast.LENGTH_SHORT).show()
+                        }else{
+                            val intent = Intent(requireContext(), DetailPaymentActivity::class.java)
+                            startActivity(intent)
+                            dismiss()
+                        }
+
+                    }
+                }
+            }
+
+    }
+
     private fun requestDetailClassFromApi() {
 
         if (COURSE_ID != null) {
---
+++ app/src/main/java/com/programmer/finalproject/ui/fragment/DetailPaymentActivity.kt
@@ -0,0 +1,12 @@
+package com.programmer.finalproject.ui.fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.programmer.finalproject.R
+
+class DetailPaymentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_payment)
+    }
+}
