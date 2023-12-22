f2639435ceb881676aeea965abfc0e1f1860281a
LailyNurH <lailyhamidah3005@gmail.com>
2023-12-22 15:35:28 +0700

    detail payment activity layout

--- app/src/main/java/com/programmer/finalproject/ui/fragment/DetailPaymentActivity.kt
+++ app/src/main/java/com/programmer/finalproject/ui/fragment/DetailPaymentActivity.kt
@@ -2,11 +2,99 @@ package com.programmer.finalproject.ui.fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
+import androidx.transition.AutoTransition
+import androidx.transition.TransitionManager
 import com.programmer.finalproject.R
+import com.programmer.finalproject.databinding.ActivityDetailKelasBinding
+import com.programmer.finalproject.databinding.ActivityDetailPaymentBinding
+import com.programmer.finalproject.databinding.FragmentDetailPaymentBinding
 
 class DetailPaymentActivity : AppCompatActivity() {
+    private var expandedLayoutBank = false
+    private var expandedLayoutCredit = false
+    private lateinit var binding: ActivityDetailPaymentBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_payment)
+        binding = ActivityDetailPaymentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupExpandableLayout(
+            binding.cvBank,
+            binding.ivExpand,
+            binding.expandLayout,
+            R.drawable.ic_expand_less,
+            R.drawable.ic_expand,
+            expandedLayoutBank
+        ) {
+            expandedLayoutBank = it
+            if (it) {
+                collapseExpandableLayout(
+                    binding.cvCreditCard,
+                    binding.ivExpandCredit,
+                    binding.expandLayoutCredit,
+                    R.drawable.ic_expand
+                )
+            }
+        }
+
+        setupExpandableLayout(
+            binding.cvCreditCard,
+            binding.ivExpandCredit,
+            binding.expandLayoutCredit,
+            R.drawable.ic_expand_less,
+            R.drawable.ic_expand,
+            expandedLayoutCredit
+        ) {
+            expandedLayoutCredit = it
+            if (it) {
+                collapseExpandableLayout(
+                    binding.cvBank,
+                    binding.ivExpand,
+                    binding.expandLayout,
+                    R.drawable.ic_expand
+                )
+            }
+        }
+    }
+    private fun setupExpandableLayout(
+        cardView: CardView,
+        arrow: ImageView,
+        expandLayout: View,
+        expandLessIcon: Int,
+        expandIcon: Int,
+        expandedLayout: Boolean,
+        onStateChanged: (Boolean) -> Unit
+    ) {
+        arrow.setOnClickListener {
+            val transition = AutoTransition()
+            TransitionManager.beginDelayedTransition(cardView, transition)
+            if (expandLayout.visibility == View.VISIBLE) {
+                expandLayout.visibility = View.GONE
+                arrow.setImageResource(expandIcon)
+                onStateChanged(false)
+            } else {
+                expandLayout.visibility = View.VISIBLE
+                arrow.setImageResource(expandLessIcon)
+                onStateChanged(true)
+            }
+        }
+
+        expandLayout.visibility = if (expandedLayout) View.VISIBLE else View.GONE
+        arrow.setImageResource(if (expandedLayout) expandLessIcon else expandIcon)
+    }
+
+    private fun collapseExpandableLayout(
+        cardView: CardView,
+        arrow: ImageView,
+        expandLayout: View,
+        expandIcon: Int
+    ) {
+        val transition = AutoTransition()
+        TransitionManager.beginDelayedTransition(cardView, transition)
+        expandLayout.visibility = View.GONE
+        arrow.setImageResource(expandIcon)
     }
 }
